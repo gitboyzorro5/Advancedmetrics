2c2d835a184c216b91d6d0a2cda9192d511586fd
gitboyzorro5 <kibetleonard8@gmail.com>
2024-02-05 20:56:11 +0300

    added squad stats

---
+++ SquadPicks.R
@@ -0,0 +1,176 @@
+library('xlsx')
+#E0
+
+unlink('Squads/E0/*')
+for(e0_sn in 1:19){
+  df <- eplsquad[eplsquad$Squad == final_doublefixture_e0[e0_sn,1],]
+
+  df2 <- eplsquad[eplsquad$Squad == final_doublefixture_e0[e0_sn + 1,1],]
+
+  temp_players <- rbind(df,df2)
+  temp_players <- temp_players[order(temp_players$YCrd, decreasing = T),]
+
+  path = "C:\\Users\\Kovan\\Advancedmetrics\\Squads\\E0"
+  write.xlsx(temp_players,file.path(path,paste(final_doublefixture_e0[e0_sn,1],final_doublefixture_e0[e0_sn + 1,1],"squad.xlsx",sep = "_")))
+
+}
+
+#D1
+unlink('Squads/D1/*')
+for(d1_sn in 1:17){
+  df <- bundesligasquad[bundesligasquad$Squad == final_doublefixture_d1[d1_sn,1],]
+
+  df2 <- bundesligasquad[bundesligasquad$Squad == final_doublefixture_d1[d1_sn + 1,1],]
+
+  temp_players <- rbind(df,df2)
+  temp_players <- temp_players[order(temp_players$YCrd, decreasing = T),]
+
+  path = "C:\\Users\\Kovan\\Advancedmetrics\\Squads\\D1"
+  write.xlsx(temp_players,file.path(path,paste(final_doublefixture_d1[d1_sn,1],final_doublefixture_d1[d1_sn + 1,1],"squad.xlsx",sep = "_")))
+
+}
+
+#I1
+unlink('Squads/I1/*')
+for(i1_sn in 1:19){
+  df <- serieasquad[serieasquad$Squad == final_doublefixture_i1[i1_sn,1],]
+
+  df2 <- serieasquad[serieasquad$Squad == final_doublefixture_i1[i1_sn + 1,1],]
+
+  temp_players <- rbind(df,df2)
+  temp_players <- temp_players[order(temp_players$YCrd, decreasing = T),]
+
+  path = "C:\\Users\\Kovan\\Advancedmetrics\\Squads\\I1"
+  write.xlsx(temp_players,file.path(path,paste(final_doublefixture_i1[i1_sn,1],final_doublefixture_i1[i1_sn + 1,1],"squad.xlsx",sep = "_")))
+
+}
+
+#SP1
+
+unlink('Squads/SP1/*')
+for(sp1_sn in 1:19){
+  df <- laligasquad[laligasquad$Squad == final_doublefixture_sp1[sp1_sn,1],]
+
+  df2 <- laligasquad[laligasquad$Squad == final_doublefixture_sp1[sp1_sn + 1,1],]
+
+  temp_players <- rbind(df,df2)
+  temp_players <- temp_players[order(temp_players$YCrd, decreasing = T),]
+
+  path = "C:\\Users\\Kovan\\Advancedmetrics\\Squads\\SP1"
+  write.xlsx(temp_players,file.path(path,paste(final_doublefixture_sp1[sp1_sn,1],final_doublefixture_sp1[sp1_sn + 1,1],"squad.xlsx",sep = "_")))
+
+}
+
+#F1
+unlink('Squads/F1/*')
+for(f1_sn in 1:17){
+  df <- ligueonesquad[ligueonesquad$Squad == final_doublefixture_f1[f1_sn,1],]
+
+  df2 <- ligueonesquad[ligueonesquad$Squad == final_doublefixture_f1[f1_sn + 1,1],]
+
+  temp_players <- rbind(df,df2)
+  temp_players <- temp_players[order(temp_players$YCrd, decreasing = T),]
+
+  path = "C:\\Users\\Kovan\\Advancedmetrics\\Squads\\F1"
+  write.xlsx(temp_players,file.path(path,paste(final_doublefixture_f1[f1_sn,1],final_doublefixture_f1[f1_sn + 1,1],"squad.xlsx",sep = "_")))
+
+}
+########################################################################################
+########################################################################################
+#write the second part of the squad
+#E0
+for(e0_sn in 1:19){
+  ef <- eplsquad2[eplsquad2$Squad == final_doublefixture_e0[e0_sn,1],]
+
+  ef2 <- eplsquad2[eplsquad2$Squad == final_doublefixture_e0[e0_sn + 1,1],]
+
+  temp_players2 <- rbind(ef,ef2)
+  temp_players2 <- temp_players2[order(temp_players2$Gls, decreasing = T),]
+
+  path = "C:\\Users\\Kovan\\Advancedmetrics\\Squads\\E0"
+  write.xlsx(temp_players2,file.path(path,paste(final_doublefixture_e0[e0_sn,1],final_doublefixture_e0[e0_sn + 1,1],"squad.xlsx",sep = "_")),sheetName = "sheet",append = TRUE)
+
+}
+########################################
+#D1
+for(d1_sn in 1:17){
+  ef <- bundesligasquad2[bundesligasquad2$Squad == final_doublefixture_d1[d1_sn,1],]
+
+  ef2 <- bundesligasquad2[bundesligasquad2$Squad == final_doublefixture_d1[d1_sn + 1,1],]
+
+  temp_players2 <- rbind(ef,ef2)
+  temp_players2 <- temp_players2[order(temp_players2$Gls, decreasing = T),]
+
+  path = "C:\\Users\\Kovan\\Advancedmetrics\\Squads\\D1"
+  write.xlsx(temp_players2,file.path(path,paste(final_doublefixture_d1[d1_sn,1],final_doublefixture_d1[d1_sn + 1,1],"squad.xlsx",sep = "_")),sheetName = "sheet",append = TRUE)
+
+}
+########################################
+#I1
+for(i1_sn in 1:19){
+  ef <- serieasquad2[serieasquad2$Squad == final_doublefixture_i1[i1_sn,1],]
+
+  ef2 <- serieasquad2[serieasquad2$Squad == final_doublefixture_i1[i1_sn + 1,1],]
+
+  temp_players2 <- rbind(ef,ef2)
+  temp_players2 <- temp_players2[order(temp_players2$Gls, decreasing = T),]
+
+  path = "C:\\Users\\Kovan\\Advancedmetrics\\Squads\\I1"
+  write.xlsx(temp_players2,file.path(path,paste(final_doublefixture_i1[i1_sn,1],final_doublefixture_i1[i1_sn + 1,1],"squad.xlsx",sep = "_")),sheetName = "sheet",append = TRUE)
+
+}
+#######################################
+#SP1
+for(sp1_sn in 1:19){
+  ef <- laligasquad2[laligasquad2$Squad == final_doublefixture_sp1[sp1_sn,1],]
+
+  ef2 <- laligasquad2[laligasquad2$Squad == final_doublefixture_sp1[sp1_sn + 1,1],]
+
+  temp_players2 <- rbind(ef,ef2)
+  temp_players2 <- temp_players2[order(temp_players2$Gls, decreasing = T),]
+
+  path = "C:\\Users\\Kovan\\Advancedmetrics\\Squads\\SP1"
+  write.xlsx(temp_players2,file.path(path,paste(final_doublefixture_sp1[sp1_sn,1],final_doublefixture_sp1[sp1_sn + 1,1],"squad.xlsx",sep = "_")),sheetName = "sheet",append = TRUE)
+
+}
+
+#######################################
+#F1
+for(f1_sn in 1:17){
+  ef <- ligueonesquad2[ligueonesquad2$Squad == final_doublefixture_f1[f1_sn,1],]
+
+  ef2 <- ligueonesquad2[ligueonesquad2$Squad == final_doublefixture_f1[f1_sn + 1,1],]
+
+  temp_players2 <- rbind(ef,ef2)
+  temp_players2 <- temp_players2[order(temp_players2$Gls, decreasing = T),]
+
+  path = "C:\\Users\\Kovan\\Advancedmetrics\\Squads\\F1"
+  write.xlsx(temp_players2,file.path(path,paste(final_doublefixture_f1[f1_sn,1],final_doublefixture_f1[f1_sn + 1,1],"squad.xlsx",sep = "_")),sheetName = "sheet",append = TRUE)
+
+}
+
+
+
+
+################individual team name#######################################
+#df <- playerdata[playerdata$Squad == "Getafe",]
+#df2 <- playerdata[playerdata$Squad == "Real Madrid",]
+#temp_players <- rbind(df,df2)
+#temp_players <- temp_players[order(temp_players$Ycd, decreasing = T),]
+#write.xlsx(temp_players,'Temp/getafevsreal.xlsx')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ divisions.R
@@ -0,0 +1,69 @@
+#by gitboyzorro5
+#create divisions and order by date
+library('lubridate')
+allteams20232024 <- read.csv('allteams20232024SOT.csv')
+
+#change date strings to Date objects
+allteams20232024$Date <- dmy(allteams20232024$Date)
+allteams20232024 <- allteams20232024[order(as.Date(allteams20232024$Date, format = "%d/%m/%Y"), decreasing = FALSE),]
+#calculate total goals
+allteams20232024$TG <- allteams20232024$FTHG + allteams20232024$FTAG
+allteams20232024$TC <- allteams20232024$HCO + allteams20232024$ACO
+allteams20232024$TF <- allteams20232024$HF + allteams20232024$AF
+allteams20232024$COSC <- paste(allteams20232024$HCO,allteams20232024$ACO,sep = "-")
+allteams20232024$OV15 <- ifelse(allteams20232024$TG >= 2,"Y","N")
+allteams20232024$OV25 <- ifelse(allteams20232024$TG >= 3,"Y","N")
+allteams20232024$OV35 <- ifelse(allteams20232024$TG >= 4,"Y","N")
+allteams20232024$TY <- allteams20232024$HY + allteams20232024$AY
+allteams20232024$TR <- allteams20232024$HR + allteams20232024$AR
+
+#create divisions subsets
+B1 <- subset(allteams20232024, Div == "B1")
+D1 <- subset(allteams20232024, Div == "D1")
+D2 <- subset(allteams20232024, Div == "D2")
+E0 <- subset(allteams20232024, Div == "E0")
+E1 <- subset(allteams20232024, Div == "E1")
+E2 <- subset(allteams20232024, Div == "E2")
+E3 <- subset(allteams20232024, Div == "E3")
+EC <- subset(allteams20232024, Div == "EC")
+F1 <- subset(allteams20232024, Div == "F1")
+F2 <- subset(allteams20232024, Div == "F2")
+G1 <- subset(allteams20232024, Div == "G1")
+I1 <- subset(allteams20232024, Div == "I1")
+I2 <- subset(allteams20232024, Div == "I2")
+N1 <- subset(allteams20232024, Div == "N1")
+P1 <- subset(allteams20232024, Div == "P1")
+SC0 <- subset(allteams20232024, Div == "SC0")
+SC1 <- subset(allteams20232024, Div == "SC1")
+SC2 <- subset(allteams20232024, Div == "SC2")
+SC3 <- subset(allteams20232024, Div == "SC3")
+SP1 <- subset(allteams20232024, Div == "SP1")
+SP2 <- subset(allteams20232024, Div == "SP2")
+T1 <- subset(allteams20232024, Div == "T1")
+
+#################################################################
+b1_teams <- sort(unique(B1$HomeTeam))
+d1_teams <- sort(unique(D1$HomeTeam))
+d2_teams <- sort(unique(D2$HomeTeam))
+e0_teams <- sort(unique(E0$HomeTeam))
+e1_teams <- sort(unique(E1$HomeTeam))
+e2_teams <- sort(unique(E2$HomeTeam))
+e3_teams <- sort(unique(E3$HomeTeam))
+ec_teams <- sort(unique(EC$HomeTeam))
+f1_teams <- sort(unique(F1$HomeTeam))
+f2_teams <- sort(unique(F2$HomeTeam))
+g1_teams <- sort(unique(G1$HomeTeam))
+i1_teams <- sort(unique(I1$HomeTeam))
+i2_teams <- sort(unique(I2$HomeTeam))
+n1_teams <- sort(unique(N1$HomeTeam))
+p1_teams <- sort(unique(P1$HomeTeam))
+sc0_teams <- sort(unique(SC0$HomeTeam))
+sc1_teams <- sort(unique(SC1$HomeTeam))
+sc2_teams <- sort(unique(SC2$HomeTeam))
+sc3_teams <- sort(unique(SC3$HomeTeam))
+sp1_teams <- sort(unique(SP1$HomeTeam))
+sp2_teams <- sort(unique(SP2$HomeTeam))
+t1_teams <- sort(unique(T1$HomeTeam))
+
+
+
